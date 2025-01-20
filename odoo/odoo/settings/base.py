04eac98ebae226bd1b084f5bc85d25b9004925db
deaxparadox <nitish136900kushwaha@gmail.com>
2025-01-20 12:29:36 +0530

    redis settings

--- odoo/odoo/settings/base.py
+++ odoo/odoo/settings/base.py
@@ -152,3 +152,10 @@ REST_FRAMEWORK = {
     # }
 }
 
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+    }
+}
