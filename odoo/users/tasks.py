44b56ad0141ec0aa3715348168b00df5a4813065
deaxparadox <nitish136900kushwaha@gmail.com>
2025-01-24 18:12:14 +0530

    working: new address model with functinality added

--- odoo/users/tasks.py
+++ odoo/users/tasks.py
@@ -1,5 +1,7 @@
 from celery import shared_task
 from celery.utils.log import get_task_logger
+from rest_framework_simplejwt.token_blacklist.models import BlacklistedToken, OutstandingToken
+from rest_framework_simplejwt.utils import aware_utcnow
 
 logger = get_task_logger(__name__)
 
@@ -7,3 +9,8 @@ logger = get_task_logger(__name__)
 @shared_task
 def sample_task():
     logger.info("The sample task just ran.")
+    
+@shared_task
+def removed_expired_tokens():
+    print("Removing expired Outstanding tokens")
+    OutstandingToken.objects.filter(expires_at__lte=aware_utcnow())
